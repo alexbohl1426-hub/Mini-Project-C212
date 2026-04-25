93c5630309dbc164513ba26f8beacb4aee12fc7f
ambohl <ambohl@iu.edu>
2026-04-24 20:00:08 -0400

    first commit

---
+++ src/ImageEditor.java
@@ -0,0 +1,236 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.List;
+import java.util.ArrayList;
+
+class ImageEditor extends JPanel {
+
+    private final Stack<BufferedImage> UNDO_STACK;
+    private final Stack<BufferedImage> REDO_STACK;
+    private final JPanel IMAGE_PANEL;
+    private final JMenuBar MENU_BAR;
+    private final JScrollPane SCROLL_PANE;
+    private final ShortcutKeyMap SHORTCUT_KEY_MAP;
+    private final ZoomMouseEventListener ZOOM_LISTENER;
+    private int zoomImageIndex;
+
+    ImageEditor() {
+        this.UNDO_STACK = new Stack<>();
+        this.REDO_STACK = new Stack<>();
+        this.SHORTCUT_KEY_MAP = new ShortcutKeyMap(this);
+        this.IMAGE_PANEL = new ImagePanel(this);
+        this.SCROLL_PANE = new JScrollPane(this.IMAGE_PANEL, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        this.MENU_BAR = new MenuBar(this);
+        this.ZOOM_LISTENER = new ZoomMouseEventListener(this, this.IMAGE_PANEL);
+        this.zoomImageIndex = 0;
+        this.setLayout(new BorderLayout());
+        this.add(this.MENU_BAR, BorderLayout.NORTH);
+        this.add(this.SCROLL_PANE, BorderLayout.CENTER);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        this.IMAGE_PANEL.repaint();
+    }
+
+    @Override
+    public void revalidate() {
+        super.revalidate();
+        if (this.IMAGE_PANEL != null) {
+            this.IMAGE_PANEL.revalidate();
+        }
+    }
+
+    /**
+     * Open a PPM Image file, read the data into a new BufferedImage, then return the image
+     *
+     * @param in - the filename of the PPM image
+     */
+    void readPpmImage(String in) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(in));
+            String line;
+            List<String> tokens = new ArrayList<>();
+
+            while ((line = br.readLine()) != null) {
+                line = line.trim();
+                if (line.length() == 0){
+                    continue;
+                }
+                if (line.startsWith("#")){
+                    continue;    /
+                }
+
+                // split tokens
+                String[] parts = line.split("\\s+");
+                for (String p : parts) {
+                    if (p.length() > 0){
+                        tokens.add(p);
+                    }
+                }
+            }
+
+            int index = 0;
+
+            String magic = tokens.get(index++);
+            if (!magic.equals("P3")) {
+                throw new IOException("Not a P3 PPM file");
+            }
+
+            int width = Integer.parseInt(tokens.get(index++));
+            int height = Integer.parseInt(tokens.get(index++));
+            int maxVal = Integer.parseInt(tokens.get(index++));
+
+            BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    int r = Integer.parseInt(tokens.get(index++));
+                    int g = Integer.parseInt(tokens.get(index++));
+                    int b = Integer.parseInt(tokens.get(index++));
+
+                    Color c = new Color(r, g, b);
+                    img.setRGB(x, y, c.getRGB());
+                }
+            }
+
+            this.UNDO_STACK.clear();
+            this.REDO_STACK.clear();
+            this.zoomImageIndex = 0;
+            this.addImage(img);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Given a file name, this method opens the file and write out the PPM header data and then the image pixel data
+     *
+     * @param out - the filename that we are writing the PPM header and image data
+     */
+    void writePpmImage(String out) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(out))) {
+
+            BufferedImage img = this.getImage();
+            bw.write("P3");
+            bw.newLine();
+            bw.write(img.getWidth() + " " + img.getHeight());
+            bw.newLine();
+            bw.write("255");
+            bw.newLine();
+            for (int y = 0; y < img.getHeight(); y++) {
+                for (int x = 0; x < img.getWidth(); x++) {
+                    Color c = new Color(img.getRGB(x, y));
+
+                    bw.write(c.getRed() + " " + c.getGreen() + " " + c.getBlue());
+
+                    if (x < img.getWidth() - 1) {
+                        bw.write(" ");
+                    }
+                }
+                bw.newLine();
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Adds a new image to the editor and the undo stack. It is assumed that the image
+     * being passed is not zoomed. If so, use the other addImage method.
+     *
+     * @param img image to add.
+     */
+    void addImage(BufferedImage img) {
+        this.UNDO_STACK.push(img);
+        this.REDO_STACK.clear();
+        this.revalidate();
+        this.repaint();
+        this.zoomImageIndex++;
+    }
+
+    /**
+     * Adds a new zoomed image to the editor. Because we only want to apply transformations
+     * to non-zoomed images, we need to keep track of where the last non-zoomed image is in
+     * the undo stack.
+     *
+     * @param img    image to add.
+     * @param zoomed flag indicating whether the image is zoomed. This is always true.
+     */
+    void addImage(BufferedImage img, boolean zoomed) {
+        this.UNDO_STACK.push(img);
+        this.REDO_STACK.clear();
+        this.revalidate();
+        this.repaint();
+        if (!zoomed) {
+            this.zoomImageIndex++;
+        }
+    }
+
+    /**
+     * Removes the current image from the editor and the undo stack.
+     * The undone image is pushed to the redo stack. If there are no images
+     * to undo, this method does nothing.
+     */
+    void undoImage() {
+        if (!this.UNDO_STACK.isEmpty()) {
+            this.REDO_STACK.push(this.UNDO_STACK.pop());
+            this.revalidate();
+            this.repaint();
+        }
+    }
+
+    /**
+     * Redoes the last undone image. The redone image is pushed to the undo stack.
+     * If there are no images to redo, this method does nothing.
+     */
+    void redoImage() {
+        if (!this.REDO_STACK.isEmpty()) {
+            this.UNDO_STACK.push(this.REDO_STACK.pop());
+            this.revalidate();
+            this.repaint();
+        }
+    }
+
+    Stack<BufferedImage> getUndoStack() {
+        return this.UNDO_STACK;
+    }
+
+    Stack<BufferedImage> getRedoStack() {
+        return this.REDO_STACK;
+    }
+
+    BufferedImage getImage() {
+        return this.UNDO_STACK.isEmpty() ? null : this.UNDO_STACK.peek();
+    }
+
+    BufferedImage getOriginalImage() {
+        if (this.zoomImageIndex < 1 || this.zoomImageIndex >= this.UNDO_STACK.size()) {
+            return null;
+        } else {
+            return this.UNDO_STACK.elementAt(this.zoomImageIndex - 1);
+        }
+    }
+
+    MenuBar getMenuBar() {
+        return (MenuBar) MENU_BAR;
+    }
+
+    JScrollPane getScrollPane() {
+        return this.SCROLL_PANE;
+    }
+
+    ZoomMouseEventListener getZoomListener() {
+        return this.ZOOM_LISTENER;
+    }
+}
---
+++ src/ImageOperations.java
@@ -0,0 +1,161 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+class ImageOperations {
+
+    /**
+     * Method removes any red to a image by setting the red to 0 for every pixel in the image
+     *
+     * @param img - the image that we are removing the red in the image
+     * @return an image with zero red for every pixel of the image
+     */
+    static BufferedImage zeroRed(BufferedImage img) {
+        // TODO.
+        BufferedImage newImg = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+        for(int i = 0; i < img.getHeight(); i++){
+            for(int j = 0; j < img.getWidth(); j++){
+                Color readColor = new Color(img.getRGB(i,j));
+                Color newColor = new Color(0, readColor.getGreen(), readColor.getBlue());
+                newImg.setRGB(i,j,newColor.getRGB());
+            }
+        }
+        return newImg;
+    }
+
+    /**
+     * Method gets rid of all the color in a image and makes it grey (or black/white techinally) by adding red green and blue
+     * and dividing by 3 for each pixel.
+     * @param img - image that is losing its color and turning grey every pixel
+     * @return a BufferedImage object that is the image that has no color and is in black/white
+     */
+    static BufferedImage grayscale(BufferedImage img) {
+
+        BufferedImage newImg = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+        for(int i = 0; i < img.getHeight(); i++){
+            for(int j = 0; j < img.getWidth(); j++){
+                Color readColor = new Color(img.getRGB(i,j));
+                int grey = (readColor.getRed() + readColor.getGreen() + readColor.getBlue()) / 3;
+                Color newColor = new Color(grey, grey, grey);
+                newImg.setRGB(i,j, newColor.getRGB());
+            }
+        }
+        return newImg;
+    }
+
+    /**
+     * Changes each color for every pixel to the opposite of its 0-255 scale. So for every color, we do 255- the color
+     * @param img - the image that is given to invert
+     * @return A image the returns the opposite for each color of every pixel
+     */
+    static BufferedImage invert(BufferedImage img) {
+
+        BufferedImage newImg = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+        for(int i = 0; i < img.getHeight(); i++){
+            for(int j = 0; j < img.getWidth(); j++){
+                Color readColor = new Color(img.getRGB(i, j));
+                Color newColor = new Color(255-readColor.getRed(), 255- readColor.getGreen(), 255 - readColor.getBlue());
+                newImg.setRGB(i,j, newColor.getRGB());
+            }
+        }
+        return newImg;
+    }
+
+    /**
+     * Flipping the image, either vertically or horizontally. If it's vertical, we flip every pixel from left to right
+     * (top and bottom stau the same). If we flip horizontally, top and bottom pixels flip (left and right stau the same0
+     * @param img - the image that is being mirrored either vertically or horizontally
+     * @param dir - the direction that we are mirroring the image
+     * @return a mirror from the original image that is either vertically or horizontally mirrored
+     */
+    static BufferedImage mirror(BufferedImage img, MirrorMenuItem.MirrorDirection dir) {
+        // TODO instantiate newImg with the *correct* dimensions.
+        BufferedImage newImg = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+        for(int i = 0; i < img.getHeight(); i++){
+            for(int j = 0; j < img.getWidth(); j++){
+                if (dir == MirrorMenuItem.MirrorDirection.VERTICAL) {
+                    newImg.setRGB(img.getWidth() - 1 - j, i, img.getRGB(i,j));
+                } else {
+                    newImg.setRGB(j, img.getWidth() - 1 - i, img.getRGB(i,j));
+                }
+            }
+        }
+
+        return newImg;
+    }
+
+    /**
+     * Method rotates the image 90 degrees counterclockwise or clockwise depending on the param dir. If a image is wide,
+     * the image will become tall. If a image is tall, the image will become wide. We rotate the pixels by moving them to new
+     * coordinates by making rows into columns and columns into rows
+     * @param img - the image that we are rotating 90 degrees counterclockwise or clockwise
+     * @param dir - the direction that we are rotating the image 90 degrees (clockwise or counterclockwise)
+     * @return a rotation from the original image based on the direction
+     */
+    static BufferedImage rotate(BufferedImage img, RotateMenuItem.RotateDirection dir) {
+        // TODO instantiate newImg with the *correct* dimensions.
+        BufferedImage newImg = new BufferedImage(img.getHeight(), img.getWidth(), BufferedImage.TYPE_INT_RGB);
+        for(int i = 0; i < img.getHeight(); i++){
+            for(int j = 0; j < img.getWidth(); j++){
+                if (dir == RotateMenuItem.RotateDirection.CLOCKWISE) {
+                    newImg.setRGB(i, (img.getWidth() - 1) - j, img.getRGB(j,i));
+                } else {
+                    newImg.setRGB((img.getHeight() - 1) - i, j, img.getRGB(j,i));
+                }
+            }
+        }
+
+        return newImg;
+    }
+
+    /**
+     * Method copies the pixels either horizontally or vertically a certain amount of times (which is what n is). If it
+     * is a horizontal repeat, we copy the width n times. If it is a vertical repeat, we copy the height n times
+     * @param img - the image that we are repeating
+     * @param n  - the number of times we repeat the image (n * height, n * weight depending on the direction).
+     * @param dir - the direction that we are repeating the image
+     * @return - a new image that is repeating either horizontally or vertically the same image a n amount of times
+     */
+    static BufferedImage repeat(BufferedImage img, int n, RepeatMenuItem.RepeatDirection dir) {
+        BufferedImage newImg = null;
+        if (dir == RepeatMenuItem.RepeatDirection.HORIZONTAL) {
+           newImg = new BufferedImage(img.getWidth() * n, img.getHeight(), BufferedImage.TYPE_INT_RGB);
+           for(int i = 0; i < n; i++){
+               for(int j = 0; j < img.getHeight(); j++){
+                   for(int k = 0; k < img.getWidth(); k++){
+                       newImg.setRGB(i + k * img.getWidth(), j, img.getRGB(k,j));
+                   }
+               }
+           }
+        } else {
+            newImg = new BufferedImage(img.getWidth(), img.getHeight() * n, BufferedImage.TYPE_INT_RGB);
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < img.getHeight(); j++){
+                    for(int k = 0; k < img.getWidth(); k++){
+                        newImg.setRGB( k, i+j * img.getHeight(), img.getRGB(k,j));
+                    }
+                }
+            }
+        }
+        return newImg;
+    }
+
+    /**
+     * Zooms in on the image. The zoom factor increases in multiplicatives of 10% and
+     * decreases in multiplicatives of 10%.
+     *
+     * @param img        the original image to zoom in on. The image cannot be already zoomed in
+     *                   or out because then the image will be distorted.
+     * @param zoomFactor The factor to zoom in by.
+     * @return the zoomed in image.
+     */
+    static BufferedImage zoom(BufferedImage img, double zoomFactor) {
+        int newImageWidth = (int) (img.getWidth() * zoomFactor);
+        int newImageHeight = (int) (img.getHeight() * zoomFactor);
+        BufferedImage newImg = new BufferedImage(newImageWidth, newImageHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = newImg.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2d.drawImage(img, 0, 0, newImageWidth, newImageHeight, null);
+        g2d.dispose();
+        return newImg;
+    }
+}
